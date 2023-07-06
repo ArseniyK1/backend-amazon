b69bcb8195d54498d2991df48c30c10818463d80
ArseniyK1 <qwertototo2332@gmail.com>
2023-07-06 21:16:28 +0300

    commm

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,8 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  //
+  //ArseniyK1/fsdfs
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
