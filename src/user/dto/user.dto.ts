16d80ac7d9f1d832a4cfc3e0b0da7bb17e01bdbc
ArseniyK1 <qwertototo2332@gmail.com>
2023-07-16 14:50:57 +0300

    add user dto

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class UserDto {
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  avatarPath: string;
+
+  @IsString()
+  @IsOptional()
+  phone: string;
+}
+//
