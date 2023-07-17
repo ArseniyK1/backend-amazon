1fd952d32bfabf201d8ecbcfa25c1548436042a9
ArseniyK1 <qwertototo2332@gmail.com>
2023-07-17 20:56:23 +0300

    rewrtite seeder - remove old func faker

--- sedeer/seed.ts
+++ sedeer/seed.ts
@@ -1,8 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { PrismaClient, Product } from '@prisma/client';
 import * as dotenv from 'dotenv';
-import { generateSlug } from 'src/utils/generate-slug';
-import { getRandomNumber } from 'src/utils/random-number';
 
 dotenv.config();
 const prisma = new PrismaClient();
@@ -17,16 +15,17 @@ const createProducts = async (quantity: number) => {
     const product = await prisma.product.create({
       data: {
         name: productName,
-        slug: generateSlug(productName),
+        slug: faker.helpers.slugify(productName),
         description: faker.commerce.productDescription(),
-        price: +faker.commerce.price(10, 999, 0),
-        images: Array.from({ length: getRandomNumber(2, 6) }).map(() =>
-          faker.image.url(),
-        ),
+        price: +faker.commerce.price({ min: 10, max: 999, dec: 0 }),
+
+        images: Array.from({
+          length: faker.number.int({ min: 2, max: 6 }),
+        }).map(() => faker.image.url()),
         category: {
           create: {
             name: categoryName,
-            slug: generateSlug(categoryName),
+            slug: faker.helpers.slugify(categoryName),
           },
         },
         reviews: {
