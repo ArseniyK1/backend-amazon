f8bf690ab7415940005fcca42dc6768d739f5eea
ArseniyK1 <qwertototo2332@gmail.com>
2023-07-19 01:24:49 +0300

    add crud for category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,62 @@
-import { Controller } from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 import { CategoryService } from './category.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Put,
+  UsePipes,
+  ValidationPipe,
+  Post,
+  Delete,
+} from '@nestjs/common';
+import { CategoryDto } from './dto/category.dto';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async getAll() {
+    return this.categoryService.getAll();
+  }
+
+  @Get('by-slug/:slug')
+  async getBySlug(@Param('slug') slug: string) {
+    return this.categoryService.bySlug(slug);
+  }
+
+  @Get(':id')
+  @Auth()
+  async getById(@Param('id') id: string) {
+    return this.categoryService.byId(+id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put(':categoryId')
+  async update(
+    @Param('categoryId') categoryId: string,
+    @Body() dto: CategoryDto,
+  ) {
+    return this.categoryService.update(+categoryId, dto);
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Post()
+  async create() {
+    return this.categoryService.create();
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Delete('/:categoryId')
+  async delete(@Param('categoryId') categoryId: string) {
+    return this.categoryService.delete(+categoryId);
+  }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
+
+import { returnCategoryObject } from './return-category.object';
+import { PrismaService } from 'src/prisma.service';
+import { CategoryDto } from './dto/category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async byId(id: number) {
+    const category = this.prisma.category.findUnique({
+      where: {
+        id,
+      },
+      select: returnCategoryObject,
+    });
+
+    if (!category) throw new Error('Category not found');
+
+    return category;
+  }
+
+  async bySlug(slug: string) {
+    const category = this.prisma.category.findUnique({
+      where: {
+        slug,
+      },
+      select: returnCategoryObject,
+    });
+
+    if (!category) throw new NotFoundException('Category not found');
+
+    return category;
+  }
+
+  async getAll() {
+    return this.prisma.category.findMany({
+      select: returnCategoryObject,
+    });
+  }
+
+  async create() {
+    return this.prisma.category.create({
+      data: {
+        name: '',
+        slug: '',
+      },
+    });
+  }
+
+  async update(id: number, dto: CategoryDto) {
+    return this.prisma.category.update({
+      where: {
+        id,
+      },
+      data: {
+        name: dto.name,
+        slug: faker.helpers.slugify(dto.name).toLowerCase(),
+      },
+    });
+  }
+
+  async delete(id: number) {
+    return this.prisma.category.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
