5863845db647178a542cecfe11062cc6482996b1
ArseniyK1 <qwertototo2332@gmail.com>
2023-07-19 16:10:02 +0300

    added crud for statistics (костыль)

--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class StatisticsService {}
+export class StatisticsService {
+  constructor(
+    private prisma: PrismaService,
+    private user: UserService,
+  ) {}
+
+  async getMain(userId: number) {
+    const user = await this.user.byId(userId, {
+      orders: {
+        select: {
+          items: true,
+        },
+      },
+      reviews: true,
+    });
+
+    // for (let order of user.orders) {
+    //   let total = 0
+    //   for (let vote of order.votes) {
+    //     vote_sum += vote.value
+    //   }
+    //   ;(question as any).vote_sum = vote_sum
+    // }
+
+    return [
+      {
+        name: 'Orders',
+        value: user.orders.length,
+      },
+      {
+        name: 'Reviews',
+        value: user.reviews.length,
+      },
+      {
+        name: 'Favorites',
+        value: user.favorites.length,
+      },
+      {
+        name: 'Total amount',
+        value: 1000,
+      },
+    ];
+  }
+}
